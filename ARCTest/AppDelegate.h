96a92f718d00d4c85286967e28b03dbd31ba5163
Naum Puroski <nomenas@Naums-MacBook-Pro.local>
2014-07-17 00:09:32 +0200

    Initial Commit

---
+++ ARCTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ARCTest
+//
+//  Created by Naum Puroski on 7/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ARCTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ARCTest
+//
+//  Created by Naum Puroski on 7/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
