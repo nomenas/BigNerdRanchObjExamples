c2d7ebd7c006c087d49814acf02bd871915b7104
Naum Puroski <nomenas@Naums-MacBook-Pro.local>
2014-09-18 00:34:51 +0200

    Hypnosister example

---
+++ BigNerdRanchExamples/Hypnosister/Hypnosister/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Naum Puroski on 9/16/14.
+//  Copyright (c) 2014 purocomputers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
